1f00b3f6a9fd3d698078965c96535c4d65632ac2
MdArslaan <142446633+MdArslaan@users.noreply.github.com>
2023-10-03 01:12:11 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+       colors: {
+        "primary-color": "#0EC54E",
+        "secondary-color": "#16113A",
+        "accent": "#e9e7f9",
+      
+      },
+    },
+  },
+  plugins: [],
+}
+
